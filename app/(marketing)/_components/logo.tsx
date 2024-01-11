046c1545e261ad851fbee3d9a9e05ec9bb4cbb22
sawan1k <sawankushwaha222@gmail.com>
2024-01-11 12:51:12 +0530

    value change

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -12,18 +12,18 @@ export const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
       <Image
-        src="/logo.svg"
+        src="/download.png"
         height="40"
         width="40"
         alt="Logo"
         className="dark:hidden"
       />
       <Image
-        src="/logo-dark.svg"
+        src="/download.png"
         height="40"
         width="40"
         alt="Logo"
-        className="hidden dark:block"
+        className="hidden dark:block bg-black rounded-full"
       />
       <p className={cn("font-semibold", font.className)}>Notion</p>
     </div>
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -34,14 +34,14 @@ export const Navbar = () => {
               </Button>
             </SignInButton>
             <SignInButton mode="modal">
-              <Button size="sm">Get Jotion free</Button>
+              <Button size="sm">Get Notion free</Button>
             </SignInButton>
           </>
         )}
         {isAuthenticated && !isLoading && (
           <>
             <Button variant="ghost" size="sm" asChild>
-              <Link href="/documents">Enter Jotion</Link>
+              <Link href="/documents">Enter Notion</Link>
             </Button>
             <UserButton afterSignOutUrl="/" />
           </>
